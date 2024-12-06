f4d00a9a192458bad47a63312ebff446982e7feb
boryaxray <157354076+boryaxray@users.noreply.github.com>
2024-12-07 01:36:02 +0500

    Add files via upload

---
+++ pr8_files/pr8_files.cpp
@@ -0,0 +1,21 @@
+﻿// pr8_files.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, z;
+	setlocale(0, "rus");
+	FILE* fl;
+	if (fopen_s(&fl, "rrr.txt", "r") != NULL)
+	{
+		printf("файл невозможно открыть\n");
+	}
+	else if(fscanf_s(fl, "%d,%d,%d", &x, &y, &z) == 3) {
+			printf("x=%d, y=%d, z=%d\n", x, y, z);
+			printf("Расстояние от начала координат до точки: %f", (sqrt(pow(x,2)+pow(y,2)+pow(z,2))));
+	}
+	
+}
